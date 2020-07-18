773ea03f0d99d07669b7a22f5d1390f8847f6f6e
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-07-19 02:57:34 +0530

    Upload

---
+++ src/tax/invoice/generator/TaxInvoiceGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tax.invoice.generator;
+
+/**
+ *
+ * @author PrithviDevKumar
+ */
+public class TaxInvoiceGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
